4312f6010f384368ad7c65b36f50b43d245ce155
Cherishnoobs <44421708+Cherishnoobs@users.noreply.github.com>
2026-01-19 07:33:38 +0000

    Deploying to gh-pages from @ Cherishnoobs/Chenglong-Chu.github.io@95c4188e1072ecaa86254cbf64c683a470f4aae6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -469,6 +469,13 @@ ninja.data = [{
           window.open("/Chenglong-Chu.github.io/feed.xml", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=O_merfAAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
